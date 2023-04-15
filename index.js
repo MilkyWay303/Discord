6fb402ce2dc5f8b0717ecee79fc9c4a66e72c580
MilkyWay303 <pomidorowy999@gmail.com>
2023-04-15 12:55:19 +0200

    Animacje

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
-// // tu piszemy skrypty do interktywności
-// addEventListener("DOMContentLoaded", (event) => {
-// 	console.log("witamy");
-// 	const h1 = document.querySelector(".Napis1");
-// 	console.log(h1); // [pbranie całego elementu html]
-// 	let textH1 = h1.innerText;
-// 	console.log(textH1); // [pbranie całego elementu html]
-// 	textH1 = "Nowy discord";
-// 	h1.innerText = textH1;
+// 
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".tag");
 
-// 	let i = 0;
-// 	// addEventListener("mousemove", (event) => {
-// 	//     console.log(i)
-// 	//     i = i + 1;
-// 	// })
-// 	// index++ => index = index +1
-// 	for (let index = 1; index < 11; index++) {
-// 		// const element = array[index];
-// 		console.log(index);
-// 		setTimeout(() => {
-// 			h1.innerText = index;
-// 		}, 1000 * index);
-// 	}
-// 	// 1
-// 	// 1
-// 	// 2
-// 	// 3
-// 	// 4
-// 	// 5
-// 	// 6
-// 	// 7
-// 	// 8
-// 	// 9
-// 	// 10
-// });
+    for (var i = 0; i < tags.length; i++) {
+        var tag = tags[i];
+
+        if ($(tag).position().top < pageBottom) {
+            $(tag).addClass("visible");
+        } else {
+            $(tag).removeClass("visible");
+        }
+    }
+});
